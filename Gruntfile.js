a687a09466dbef4fa43f8ae3f8ac77c213621085
Joseph Galindo <joseph.galindo18@gmail.com>
2016-01-16 12:33:19 -0500

    Implement alternate way of loading YAML frontmatter - assemble was breaking on windows git bash

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,18 @@ module.exports = function(grunt) {
 			}
 		},
 
+		frontmatter: {
+			build: {
+				options: {
+					minify: false,
+					width:60
+				},
+				files: {
+					'src/markup/yaml.json': ['src/markup/pages/*.jade']
+				}
+			}
+		},
+
 		jade: {
 			build: {
 				options: {
@@ -19,13 +31,32 @@ module.exports = function(grunt) {
 					debug:false,
 					basedir: 'src/markup',
 					data: function(dest,src) {
-						var yfm = require('assemble-yaml');
-						//console.log('./' + src);
+
+						//read json made by frontmatter task - concats all yaml into one file
+
+						var fs = require('fs');
+						var yaml = JSON.parse(fs.readFileSync('src/markup/yaml.json','utf8'));
+
+						//get filename of current jade page currently being compiled
+
+						var filename = src[0].replace('src/markup/pages/', '').replace('.jade', '').split('/').pop();
+
+						//loop through json of all page YAMLs to find the one page YAML that matches the jade page currently being compiled
+
+						var pageYAML = null;
+
+						for(object in yaml) {
+							if(yaml[object]['section'] === filename) {
+								pageYAML = yaml[object];
+							}
+						}
+
+						//return the pageYAML found above
+
 						return {
 							from: src,
 							to: dest,
-							fileName: src[0].replace('src/markup/pages/', '').replace('.jade', '').split('/').pop(),
-							page: yfm.extractJSON('./' + src)
+							page: pageYAML
 						};
 					}
 				},
@@ -122,11 +153,12 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-autoprefixer');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-frontmatter');
 	grunt.loadNpmTasks('grunt-newer');
 
 	//defining of custom tasks for CLI use
-	grunt.registerTask('markup:build', ['jade']);
-	grunt.registerTask('markup:watch', ['newer:jade']);
+	grunt.registerTask('markup:build', ['frontmatter', 'jade']);
+	grunt.registerTask('markup:watch', ['frontmatter', 'newer:jade']);
 
 	grunt.registerTask('styles:build', ['sass', 'autoprefixer']);
 	grunt.registerTask('styles:watch', ['newer:sass', 'newer:autoprefixer']);
